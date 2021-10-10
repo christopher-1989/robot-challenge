7b93eb8a2ab779f664024712ccc90e49155d9971
Christopher McDonald <c.robin.mcdonald@gmail.com>
2021-10-10 18:26:08 +1100

    Added testing and logic for processing input commands

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
+const { stringify } = require("querystring")
 
 const inputCommands = [
     "PLACE 0,0,NORTH",
     "MOVE",
     "REPORT"
 ]
+
+const validDirections = ['NORTH', 'EAST', 'SOUTH', 'WEST']
+
+// const robots = [
+//     {
+//         x: 2,
+//         y: 2,
+//         f: "EAST"
+//     }, 
+//     {
+//         x: 2,
+//         y: 2,
+//         f: "NORTH"
+//     }, 
+//     {
+//         x: 2,
+//         y: 2,
+//         f: "SOUTH"
+//     }, 
+//     {
+//         x: 0,
+//         y: 0,
+//         f: "SOUTH"
+//     }]
+
 
 function findFirstPlacement (listOfCommands) {
     for ( let i = 0; i < listOfCommands.length; i++) {
@@ -20,14 +46,93 @@ function findFirstPlacement (listOfCommands) {
 function formatPlacementCommand (placeCommand) {
     const coordDirection = placeCommand.split(' ')[1]
     if (!coordDirection) {
-        throw new Error("Incorrect format")
+        throw new Error("Incorrect format. Expected 'PLACE X,Y,F'")
     }
-    const [X, Y, F] = coordDirection.split(',')
+    const [X, Y, F] = coordDirection.split(',');
+    intX = parseInt(X);
+    intY = parseInt(Y);
+    if(isNaN(intX) || isNaN(intY)) {
+        throw new TypeError("X and Y coordinates must be integers")
+    }
+    if(validDirections.indexOf(F) === -1) {
+        throw new Error("F must be a valid direction. Expected NORTH SOUTH EAST or WEST")
+    }
     return {
-        x: parseInt(X),
-        y: parseInt(Y),
+        x: intX,
+        y: intY,
         f: F
     }
+}
+
+function rotateLeft (robot) {
+    const currentDirection = robot.f
+    switch (currentDirection) {
+        case "NORTH": {
+            robot.f = "WEST"
+            return robot
+        }
+        case "WEST": {
+            robot.f = "SOUTH"
+            return robot
+        }
+        case "SOUTH": {
+            robot.f = "EAST"
+            return robot
+        }
+        case "EAST": {
+            robot.f = "NORTH"
+            return robot
+        }
+        default: console.error("Something went wrong")
+    }
+}
+
+function rotateRight (robot) {
+    const currentDirection = robot.f
+    switch (currentDirection) {
+        case "NORTH": {
+            robot.f = "EAST"
+            return robot
+        }
+        case "WEST": {
+            robot.f = "NORTH"
+            return robot
+        }
+        case "SOUTH": {
+            robot.f = "WEST"
+            return robot
+        }
+        case "EAST": {
+            robot.f = "SOUTH"
+            return robot
+        }
+        default: console.error("Something went wrong")
+    }
+}
+
+function report (robots, robot = []) {
+    const numberOfBots = robots.length;
+    if (numberOfBots === 0) {
+        return "There are no robots on the table."
+    } else if (numberOfBots === 1) {
+        return  `There is 1 robot on the table. It is located at ${robot.x},${robot.y}, and facing ${robot.f}`
+    } else {
+        let activeBotIndex 
+        robots.forEach((bot, index) => {
+            if (JSON.stringify(robot) === JSON.stringify(bot)) {
+                activeBotIndex = index
+            }
+        })
+    return  `There are ${numberOfBots} robots on the table.\n 
+            The active robot is number ${activeBotIndex + 1} and is located at ${robot.x},${robot.y}, and facing ${robot.f}`
+    }
+}
+
+function changeActiveRobot(robots, active) {
+    if (active > robots.length) {
+        throw new Error("Robot number is not active")
+    }
+    return robots[active-1]
 }
 
 inputCommands.forEach(command => {
@@ -39,5 +144,9 @@ inputCommands.forEach(command => {
 
 module.exports = {
     findFirstPlacement,
-    formatPlacementCommand
+    formatPlacementCommand,
+    rotateLeft,
+    rotateRight,
+    report,
+    changeActiveRobot
 }
--- test.js
+++ test.js
@@ -1,9 +1,12 @@
 const assert = require('assert');
 
-// require('./main')
 const { 
     findFirstPlacement,
-    formatPlacementCommand
+    formatPlacementCommand,
+    rotateLeft,
+    rotateRight,
+    report,
+    changeActiveRobot
  } = require('./main')
 
 describe('Inputs', () => {
@@ -34,24 +37,202 @@ describe('Inputs', () => {
         })
         describe('PLACE X,Y,F', () => {
             it("throws an error if the format doesn't have a space", () => {
-                const testInput = "PLACEXY"
+                const testInput = "PLACEXY";
                 assert.throws(() => {
-                    formatPlacementCommand(testInput)
+                    formatPlacementCommand(testInput);
                 }, {
                     name: "Error",
-                    message: "Incorrect format"
+                    message: "Incorrect format. Expected 'PLACE X,Y,F'"
                 })
             })
             it('returns an object with the keys x,y,f', () => {
-                const testInput = "PLACE 1,2,LEFT"
-                const result = formatPlacementCommand(testInput)
+                const testInput = "PLACE 1,2,SOUTH";
+                const result = formatPlacementCommand(testInput);
                 assert.deepEqual(result, {
                     x: 1,
                     y: 2,
-                    f: "LEFT"
+                    f: "SOUTH"
+                })
+            })
+            it('throws an error if x or y are not integers', () => {
+                const testInput = "PLACE x,y,LEFT";
+                assert.throws(() => {
+                    formatPlacementCommand(testInput);
+                }, {
+                    name: "TypeError",
+                    message: "X and Y coordinates must be integers"
+                })
+            })
+            it('throws an error if F is not a valid direction', () => {
+                const testInput = "PLACE 0,0,LEFT";
+                assert.throws(() => {
+                    formatPlacementCommand(testInput);
+                }, {
+                    name: "Error",
+                    message: "F must be a valid direction. Expected NORTH SOUTH EAST or WEST"
                 })
             })
         })
         
+    })
+    describe("LEFT/RIGHT rotations", () => {
+        it('LEFT for NORTH returns the direction WEST', () => {
+            const testInput = "LEFT";
+            const testRobot = {
+                x: 2,
+                y: 2,
+                f: "NORTH"
+            }
+            const result = rotateLeft(testRobot)
+            assert.deepEqual(result.f, "WEST")
+        })
+        it('LEFT for WEST returns the direction SOUTH', () => {
+            const testInput = "LEFT";
+            const testRobot = {
+                x: 2,
+                y: 2,
+                f: "WEST"
+            }
+            const result = rotateLeft(testRobot)
+            assert.deepEqual(result.f, "SOUTH")
+        })
+        it('LEFT for SOUTH returns the direction EAST', () => {
+            const testInput = "LEFT";
+            const testRobot = {
+                x: 2,
+                y: 2,
+                f: "SOUTH"
+            }
+            const result = rotateLeft(testRobot)
+            assert.deepEqual(result.f, "EAST")
+        })
+        it('LEFT for EAST returns the direction NORTH', () => {
+            const testInput = "LEFT";
+            const testRobot = {
+                x: 2,
+                y: 2,
+                f: "EAST"
+            }
+            const result = rotateLeft(testRobot)
+            assert.deepEqual(result.f, "NORTH")
+        })
+
+        it('RIGHT for NORTH returns the direction EAST', () => {
+            const testInput = "RIGHT";
+            const testRobot = {
+                x: 2,
+                y: 2,
+                f: "NORTH"
+            }
+            const result = rotateRight(testRobot)
+            assert.deepEqual(result.f, "EAST")
+        })
+        it('RIGHT for WEST returns the direction NORTH', () => {
+            const testInput = "RIGHT";
+            const testRobot = {
+                x: 2,
+                y: 2,
+                f: "WEST"
+            }
+            const result = rotateRight(testRobot)
+            assert.deepEqual(result.f, "NORTH")
+        })
+        it('RIGHT for SOUTH returns the direction WEST', () => {
+            const testInput = "RIGHT";
+            const testRobot = {
+                x: 2,
+                y: 2,
+                f: "SOUTH"
+            }
+            const result = rotateRight(testRobot)
+            assert.deepEqual(result.f, "WEST")
+        })
+        it('RIGHT for EAST returns the direction SOUTH', () => {
+            const testInput = "RIGHT";
+            const testRobot = {
+                x: 2,
+                y: 2,
+                f: "EAST"
+            }
+            const result = rotateRight(testRobot)
+            assert.deepEqual(result.f, "SOUTH")
+        })
+    })
+    describe("Report command", () => {
+        it("Prints to the console the x,y,f when there is one robot", () => {
+            const testRobot = {
+                x: 2,
+                y: 2,
+                f: "EAST"
+            }
+            const testRobots = [
+                {
+                    x: 2,
+                    y: 2,
+                    f: "EAST"
+                }
+            ]
+            
+            assert.deepEqual(report(testRobots, testRobot), `There is 1 robot on the table. It is located at 2,2, and facing EAST`)
+        })
+        it("reports no bots", () => {
+            const testRobots = []
+            assert.deepEqual(report(testRobots), "There are no robots on the table.")
+        })
+        it("Prints to the console the x,y,f when there is more than one robot", () => {
+            const testRobot = {
+                x: 0,
+                y: 0,
+                f: "NORTH"
+            }
+            const testRobots = [
+                {
+                    x: 2,
+                    y: 2,
+                    f: "EAST"
+                },
+                {
+                    x: 0,
+                    y: 0,
+                    f: "NORTH"
+                }
+            ]
+            assert.deepEqual(report(testRobots, testRobot), `There are 2 robots on the table.\n 
+            The active robot is number 2 and is located at 0,0, and facing NORTH`)
+        })
+    })
+    describe('ROBOT #', ()=> {
+        it('throws an error when the given numbered robot does not exist', ()=> {
+            const testRobots = [
+                {
+                    x: 2,
+                    y: 2,
+                    f: "EAST"
+                }
+            ]
+            assert.throws(() => {
+                changeActiveRobot(testRobots, 2)
+            },
+            {
+                name: "Error",
+                message: "Robot number is not active"
+            }
+            )
+        })
+        it('returns the new active robot', () => {
+            const testRobots = [
+                {
+                    x: 2,
+                    y: 2,
+                    f: "EAST"
+                }
+            ]
+            assert.deepEqual(changeActiveRobot(testRobots, 1), 
+            {
+                x: 2,
+                y: 2,
+                f: "EAST"
+            })
+        })
     })
 })
